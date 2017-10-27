00eca424cffb1262767602c3e5e30c6b41baa6b7
Ali Shaibu <alishaibu2002@gmail.com>
2017-10-27 15:28:50 +0100

    created API routes that allows a user to delete a recipe

--- Server/controllers/recipes.js
+++ Server/controllers/recipes.js
@@ -1,11 +1,10 @@
-const db = require('../db.json');
+import db from '../db';
 
-const recipeDb = db.recipes;
 
 let idTracker = 0;
 
 
-class Recipe { 
+class Recipe {
   static addRecipe(req, res) {
     const { recipeName, directions, ingredients } = req.body;
     const currentDate = '' + new Date();
@@ -27,12 +26,32 @@ class Recipe {
       downvotes,
       reviews,
     };
-    recipeDb.push(response);
+    db.push(response);
     res.status(200).send({ message: 'Recipe Successfully saved and created', response });
   }
   static getRecipes(req, res) {
-    res.status(200).send(recipeDb);
+    res.status(200).send(db);
+  }
+  static removeRecipes(req, res) {
+    for (let i = 0; i < db.length; i += 1) {
+      if (db[i].id === parseFloat(req.params.recipeDbid)) {
+        db.splice(i, 1);
+      }
+    }
+    res.status(200).send({ message: 'Recipe deleted' });
+  }
+
+  static updateRecipes(req, res) {
+    for (let i = 0; i <= db.length; i += 1) {
+      if (db[i].recipeDbid === req.params.recipeDbid) {
+        db[i].name = req.body.name || db.name;
+        db[i].description = req.body.description || db.description;
+        db[i].image = req.body.image || db.image;
+        db[i].ingredients = req.body.ingredients || db.ingredients;
+        db[i].directions = req.body.directions || db.directions;
+      }
+    }
+    res.status(200).send({ message: 'Successfully Updated' });
   }
 }
-
 export default Recipe;
