09eaf0e57a58ba13215726bd8ba37372b709f850
Ali Shaibu <alishaibu2002@gmail.com>
2018-01-31 23:35:43 +0100

    create recipe.js model and  migration files to work with postgres db

---
+++ Server/migrations/20180131203634-create-recipes.js
@@ -0,0 +1,66 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.createTable('Recipes', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      recipeName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      directions: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      ingredients: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      creatorId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      },
+      upvotes: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      downvotes: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      favorites: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      views: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      reviews: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface) => {
+    queryInterface.dropTable('Recipes');
+  },
+};
---
+++ Server/models/recipes.js
@@ -0,0 +1,63 @@
+export default (sequelize, DataTypes) => {
+  const Recipes = sequelize.define('Recipes', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    directions: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ingredients: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    creatorId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+      onDelete: 'SET NULL',
+      onUpdate: 'CASCADE',
+    },
+    upvotes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    downvotes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    favorites: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    views: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    reviews: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  }, {
+    classMethods: {
+      associate: () => {
+        // associations can be defined here
+      },
+    },
+  });
+  Recipes.associate = (models) => {
+    Recipes.belongsTo(models.Users, { as: 'creator', foreignKey: 'creatorId' });
+  };
+  return Recipes;
+};
