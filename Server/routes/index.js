715e88953688a7a3d2ca91c0d4271c6e54484753
Ali Shaibu <alishaibu2002@gmail.com>
2018-05-26 15:08:23 +0100

    created more routes

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -9,8 +9,12 @@ export default (app) => {
 
   app.post('/api/v1/recipes', auth, errorHandler.checkNullInput, recipeController.addRecipe);
   app.get('/api/v1/recipes', recipeController.getAllRecipes);
-  app.delete('/api/v1/recipes/:recipeId', recipeController.deleteRecipe);
-  app.put('/api/v1/recipes/:recipeId', errorHandler.checkInvalidModification, recipeController.modifyRecipe);
+  app.delete('/api/v1/recipes/:recipeId', auth, recipeController.deleteRecipe);
+  app.put('/api/v1/recipes/:recipeId', auth, errorHandler.checkInvalidModification, recipeController.modifyRecipe);
   app.post('/api/v1/recipes/:recipeId/reviews', errorHandler.checkInvalidReview, recipeController.postReview);
+  app.post('/api/v1/recipes/:recipeId/upvote', auth, recipeController.upvoteRecipe);
+  app.post('/api/v1/recipes/:recipeId/downvote', auth, recipeController.downvoteRecipe);
+  app.get('/api/v1/recipes/:recipeId', recipeController.getARecipe);
+  app.post('/api/v1/recipes/:recipeId/favorite', auth, recipeController.favoriteRecipe);
 };
 
